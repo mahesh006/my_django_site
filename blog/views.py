e065eac145c4bffaa82e597eea48fd676e8ef820
mahesh006 <mahesh_vasamsetti@srmap.edu.in>
2019-09-23 12:07:00 -0700

    add feature to edit blog posts.

--- blog/views.py
+++ blog/views.py
@@ -15,15 +15,31 @@ def post_detail(request, pk):
 	return render (request, 'blog/post_detail.html', stuff_for_frontend)
 
 def post_new(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.publised_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-        stuff_for_frontend = {'form': form}
-    return render(request, 'blog/post_edit.html', stuff_for_frontend)
+	if request.method == 'POST':
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.publised_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()
+		stuff_for_frontend = {'form': form}
+	return render(request, 'blog/post_edit.html', stuff_for_frontend)
+
+def post_edit(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == 'POST':
+		form = PostForm(request.POST,instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author =  request.user
+			post.publised_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+		stuff_for_frontend = {'form': form}
+	return render(request,'blog/post_edit.html',stuff_for_frontend)    		
+
